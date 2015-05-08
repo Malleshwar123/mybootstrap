c11063e3adb6c69f0bfdec2b0dd947f2ea5d2a26
Malleshwar123 <cmgoud@gmail.com>
2015-05-08 13:43:56 -0700

    new changes been added

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 function hello (){
 
 
-console.log ("it  am king is my begining  and wait and watch ");
+console.log ("it  am kinwar brahmananda kar and ssss 1234454 is my begining  and wait and watch ");
 }
